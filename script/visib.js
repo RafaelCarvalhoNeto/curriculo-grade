af786070885c17d01c7d5cb30062dc1d20d51b25
Rafael Carvalho <rafaelcarvalhoneto@gmail.com>
2020-07-02 19:41:50 -0300

    colocando js

---
+++ script/visib.js
@@ -0,0 +1,16 @@
+let man = document.querySelector("main")
+window.addEventListener("load", function(){
+    man.classList.add("active")
+})
+console.log(man)
+
+// main.style.visibilty = "visible"
+
+// let bod = document.body
+// bod.addEventListener("click", function(){
+//     man.classList.add("active")
+// })
+
+// window.addEventListener('load', (event) => {
+//     console.log('page is fully loaded');
+// });
